66afa0d5f210f8654e40ad7ee611ae24888e8c10
MatheusTG <matheusteodorogarcia@gmail.com>
2026-02-10 18:02:26 +0000

    test(authenticate): add authentication use case tests and test doubles
    - add AuthenticateUseCase unit tests
    - cover success authentication flow
    - cover invalid CPF authentication scenario
    - cover invalid password authentication scenario
    - add FakeEncrypter test double
    - extend FakeHasher to support hash comparison

---
+++ apps/api/src/domain/user/application/use-cases/authenticate.spec.ts
@@ -0,0 +1,73 @@
+import { FakeEncrypter } from "@test/cryptography/fake-encrypter";
+import { FakeHasher } from "@test/cryptography/fake-hasher";
+import { makeUser } from "@test/factories/make-user";
+import { InMemoryUserRepository } from "@test/repositories/in-memory-user-repository";
+import { AuthenticateUseCase } from "./authenticate";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+
+describe("Authenticate", () => {
+  let usersRepository: InMemoryUserRepository;
+  let fakeHasher: FakeHasher;
+  let fakeEncrypter: FakeEncrypter;
+
+  let sut: AuthenticateUseCase;
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUserRepository();
+
+    fakeHasher = new FakeHasher();
+    fakeEncrypter = new FakeEncrypter();
+
+    sut = new AuthenticateUseCase(usersRepository, fakeHasher, fakeEncrypter);
+  });
+
+  it("should be able to authenticate", async () => {
+    const password = "123456";
+    const user = makeUser({
+      password: await fakeHasher.hash(password),
+    });
+
+    usersRepository.items.push(user);
+
+    const result = await sut.execute({
+      cpf: user.cpf.value,
+      password: password,
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    });
+  });
+
+  it("should not be able to authenticate with an incorrect CPF", async () => {
+    const password = "123456";
+    const user = makeUser({
+      password: await fakeHasher.hash(password),
+    });
+
+    usersRepository.items.push(user);
+
+    const result = await sut.execute({
+      cpf: "incorrect-cpf",
+      password: password,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError);
+  });
+
+  it("should not be able to authenticate with an incorrect password", async () => {
+    const user = makeUser({});
+
+    usersRepository.items.push(user);
+
+    const result = await sut.execute({
+      cpf: user.cpf.value,
+      password: "incorrect-password",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError);
+  });
+});
