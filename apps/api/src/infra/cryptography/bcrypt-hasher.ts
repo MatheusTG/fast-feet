fbb9ace3fe50fe824de2a488355da51934d66e7b
MatheusTG <matheusteodorogarcia@gmail.com>
2026-02-10 22:42:40 +0000

    feat(cryptography): add JWT encrypter and hash comparer support
    - add JwtEncrypter implementation using JwtService
    - register Encrypter abstraction in CryptographyModule
    - extend BcryptHasher to support hash comparison
    - bind HashComparer to BcryptHasher implementation
    - export cryptography providers for application layer

--- apps/api/src/infra/cryptography/bcrypt-hasher.ts
+++ apps/api/src/infra/cryptography/bcrypt-hasher.ts
@@ -1,10 +1,14 @@
 import { HashGenerator } from "@/domain/user/application/cryptography/hash-generator";
-import { hash } from "bcryptjs";
+import { compare, hash } from "bcryptjs";
 
 export class BcryptHasher implements HashGenerator {
   private HASH_SALT_LENGTH = 8;
 
-  async hash(plain: string): Promise<string> {
+  hash(plain: string): Promise<string> {
     return hash(plain, this.HASH_SALT_LENGTH);
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash);
   }
 }
--- apps/api/src/infra/cryptography/cryptography.module.ts
+++ apps/api/src/infra/cryptography/cryptography.module.ts
@@ -1,9 +1,16 @@
+import { Encrypter } from "@/domain/user/application/cryptography/encrypter";
+import { HashComparer } from "@/domain/user/application/cryptography/hash-comparer";
+import { HashGenerator } from "@/domain/user/application/cryptography/hash-generator";
 import { Module } from "@nestjs/common";
 import { BcryptHasher } from "./bcrypt-hasher";
-import { HashGenerator } from "@/domain/user/application/cryptography/hash-generator";
+import { JwtEncrypter } from "./jwt-encrypter";
 
 @Module({
-  providers: [{ provide: HashGenerator, useClass: BcryptHasher }],
-  exports: [HashGenerator],
+  providers: [
+    { provide: Encrypter, useClass: JwtEncrypter },
+    { provide: HashGenerator, useClass: BcryptHasher },
+    { provide: HashComparer, useClass: BcryptHasher },
+  ],
+  exports: [Encrypter, HashGenerator, HashComparer],
 })
 export class CryptographyModule {}
