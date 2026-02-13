fa3d4ed0eedfb936dc8e97893309f77d5c3dcc69
MatheusTG <matheusteodorogarcia@gmail.com>
2026-02-13 00:11:57 +0000

    test(api, users): add comprehensive tests for ChangeUserPasswordUseCase
    - Cover successful password change scenario
    - Prevent password reuse (NewPasswordMustBeDifferentError)
    - Handle invalid user ID (ResourceNotFoundError)
    - Handle incorrect current password (InvalidCredentialsError)

---
+++ apps/api/src/domain/user/application/use-cases/change-user-password.spec.ts
@@ -0,0 +1,112 @@
+import { ResourceNotFoundError } from "@/core/errors/application/resource-not-found.error";
+import { FakeHasher } from "@test/cryptography/fake-hasher";
+import { makeUser } from "@test/factories/make-user";
+import { InMemoryUserRepository } from "@test/repositories/in-memory-user-repository";
+import { ChangeUserPasswordUseCase } from "./change-user-password";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { NewPasswordMustBeDifferentError } from "./errors/new-password-must-be-different-error";
+
+describe("Change user password", () => {
+  let usersRepository: InMemoryUserRepository;
+  let fakeHasher: FakeHasher;
+
+  let sut: ChangeUserPasswordUseCase;
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUserRepository();
+    fakeHasher = new FakeHasher();
+
+    sut = new ChangeUserPasswordUseCase(usersRepository, fakeHasher, fakeHasher);
+  });
+
+  it("should be able to change the user password", async () => {
+    const currentPassword = "123456";
+
+    const user = makeUser({
+      password: await fakeHasher.hash(currentPassword),
+    });
+
+    usersRepository.items.push(user);
+
+    const userId = user.id.toString();
+    const newPassword = "new_password";
+
+    const result = await sut.execute({
+      userId,
+      currentPassword,
+      newPassword,
+    });
+
+    const userWithNewPassword = await usersRepository.findById(userId);
+
+    expect(userWithNewPassword).toBeDefined();
+
+    if (!userWithNewPassword) return;
+
+    const isOldPasswordValid = await fakeHasher.compare(newPassword, userWithNewPassword?.password);
+
+    expect(result.isRight()).toBe(true);
+    expect(isOldPasswordValid).toBe(true);
+  });
+
+  it("should not change password back to the same password", async () => {
+    const password = "123456";
+
+    const user = makeUser({
+      password: await fakeHasher.hash(password),
+    });
+
+    usersRepository.items.push(user);
+
+    const userId = user.id.toString();
+
+    const result = await sut.execute({
+      userId,
+      currentPassword: password,
+      newPassword: password,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(NewPasswordMustBeDifferentError);
+  });
+
+  it("should not be able to change password with an invalid user ID", async () => {
+    const currentPassword = "123456";
+
+    const user = makeUser({
+      password: await fakeHasher.hash(currentPassword),
+    });
+
+    usersRepository.items.push(user);
+
+    const newPassword = "new_password";
+
+    const result = await sut.execute({
+      userId: "invalid_user_id",
+      currentPassword,
+      newPassword,
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not change password if current password is wrong", async () => {
+    const user = makeUser({
+      password: await fakeHasher.hash("123456"),
+    });
+
+    usersRepository.items.push(user);
+
+    const userId = user.id.toString();
+
+    const result = await sut.execute({
+      userId,
+      currentPassword: "incorrect_current_password",
+      newPassword: "new_password",
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(InvalidCredentialsError);
+  });
+});
