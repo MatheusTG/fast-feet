60672916d6276b5dd06fb773f8e3653002463bb5
MatheusTG <matheusteodorogarcia@gmail.com>
2026-02-18 18:44:54 +0000

    feat(api, order-repository): add findById and update methods to orders repository
    - add abstract findById and update to OrdersRepository
    - implement findById and update in PrismaOrdersRepository
    - implement findById and update in InMemoryOrdersRepository

--- apps/api/src/infra/database/prisma/repositories/prisma-orders-repository.ts
+++ apps/api/src/infra/database/prisma/repositories/prisma-orders-repository.ts
@@ -8,10 +8,35 @@ import { PrismaService } from "../prisma.service";
 export class PrismaOrdersRepository implements OrdersRepository {
   constructor(private prisma: PrismaService) {}
 
+  async findById(id: string): Promise<Order | null> {
+    const order = await this.prisma.order.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!order) {
+      return null;
+    }
+
+    return PrismaOrderMapper.toDomain(order);
+  }
+
   async create(order: Order): Promise<void> {
     const data = PrismaOrderMapper.toPrisma(order);
 
     await this.prisma.order.create({
+      data,
+    });
+  }
+
+  async update(order: Order): Promise<void> {
+    const data = PrismaOrderMapper.toPrisma(order);
+
+    await this.prisma.order.update({
+      where: {
+        id: order.id.toString(),
+      },
       data,
     });
   }
--- apps/api/test/repositories/in-memory-orders-repository.ts
+++ apps/api/test/repositories/in-memory-orders-repository.ts
@@ -4,7 +4,21 @@ import { Order } from "@/domain/logistics/enterprise/entities/order";
 export class InMemoryOrdersRepository implements OrdersRepository {
   items: Order[] = [];
 
+  async findById(id: string): Promise<Order | null> {
+    const order = this.items.find((recipient) => recipient.id.toString() === id);
+
+    return order || null;
+  }
+
   async create(order: Order): Promise<void> {
     this.items.push(order);
+  }
+
+  async update(order: Order): Promise<void> {
+    const index = this.items.findIndex((r) => r.id === order.id);
+
+    if (index !== -1) {
+      this.items[index] = order;
+    }
   }
 }
